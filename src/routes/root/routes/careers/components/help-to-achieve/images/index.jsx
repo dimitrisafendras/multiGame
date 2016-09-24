aab01303584235d9198090226079914ed7cb9bd4
Kostis Galoussis <konstantinos.galoussis@agileactors.com>
2016-09-24 17:41:23 +0300

    Move Career page implementation from Agile Actors Web App (incomplete + might not be finally included)

--- src/routes/root/routes/careers/components/help-to-achieve/images/index.jsx
+++ src/routes/root/routes/careers/components/help-to-achieve/images/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { InlineSvg } from 'components/inline-svg';
 
-export const Fun = InlineSvg(require('!!raw!./fun'));
-export const PersonalDev = InlineSvg(require('!!raw!./personal-dev'));
-export const Respect = InlineSvg(require('!!raw!./respect'));
-export const Tools = InlineSvg(require('!!raw!./tools'));
+export const Methodologies = InlineSvg(require('!!raw!./icon-agile-methodologies.svg'));
+export const UiUx = InlineSvg(require('!!raw!./icon-ux-ui.svg'));
+export const SwDevelopment = InlineSvg(require('!!raw!./icon-sw-development.svg'));
+export const Coaching = InlineSvg(require('!!raw!./icon-prof-coaching.svg'));
 
 export default {
-  Fun: <Fun />,
-  PersonalDev: <PersonalDev />,
-  Respect: <Respect />,
-  Tools: <Tools />,
+  Methodologies: <Methodologies />,
+  UiUx: <UiUx />,
+  SwDevelopment: <SwDevelopment />,
+  Coaching: <Coaching />,
 };
