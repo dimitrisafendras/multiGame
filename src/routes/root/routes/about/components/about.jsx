760fa50cab2bc642d8cb42d22c998613d3194348
Kostis Galoussis <konstantinos.galoussis@agileactors.com>
2016-09-25 15:42:01 +0300

    Create AA Vision component

--- src/routes/root/routes/about/components/about.jsx
+++ src/routes/root/routes/about/components/about.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ValueProposition from './value-proposition/value-proposition';
 import HelpToAchieve from './help-to-achieve/help-to-achieve';
-
+import AgileActorsVision from './agile-actors-vision/agile-actors-vision';
 // import LifeAtAgileActors from './life-at-agile-actors';
-// import AgileActorsVision from './agile-actors-vision';
+
 
 class About extends React.Component {
   render() {
     return (
       <section>
         <ValueProposition />
-
+        <AgileActorsVision/>
         <HelpToAchieve />
       </section>
     );
