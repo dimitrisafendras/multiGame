74386e007b4f770e49a5ebd66a1b27f3d39c82ed
Kostis Galoussis <konstantinos.galoussis@agileactors.com>
2016-09-25 16:22:16 +0300

    Create Organizations page Value Proposition component

---
+++ src/routes/root/routes/organizations/components/value-proposition/content.jsx
@@ -0,0 +1,11 @@
+//
+// The content of the component
+//
+export const content = {
+  title: 'Organizations',
+  subTitle: 'Your Business Partner for Software Development',
+  text: `From hiring the right tech professionals to untapping your team’s talent, 
+  we’re here to help you achieve your goals and grow your business. Hundreds of businesses
+   and organizations trust Agile Actors to form teams for successful projects and keep 
+   their workforce sharp.`,
+};
---
+++ src/routes/root/routes/organizations/components/value-proposition/value-proposition.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { style } from './style';
+import { content } from './content';
+import {
+  useSheet,
+  Content,
+  FlexContainer,
+  Container,
+} from 'components';
+
+//
+// The component
+//
+const ValueProposition = ({ sheet }) => {
+  const { classes } = sheet;
+
+  return (
+    <article
+      className={classes.component}>
+      <Container
+        container
+        className={classes.contentWrapper}>
+        <FlexContainer largeContainer
+          className={classes.container}
+          column>
+          <Content backgroundImage
+            className={classes.backgroundImage} />
+          <Content largeTitle
+            className={classes.title}>
+            {content.title}
+          </Content>
+          <Content largeSubTitle
+            className={classes.subtitle}>
+            {content.subTitle}
+          </Content>
+          <Content largeText
+            className={classes.text}>
+            {content.text}
+          </Content>
+        </FlexContainer>
+      </Container>
+    </article>
+  );
+};
+
+ValueProposition.propTypes = {
+  sheet: React.PropTypes.object,
+};
+
+export default useSheet(ValueProposition, style);
