ae6565aff71138b44a76a4acee10982166585340
dlivas <dimitris.livas@gmail.com>
2016-09-27 13:42:14 +0300

    update UserRoute

--- src/routes/root/routes/user/index.js
+++ src/routes/root/routes/user/index.js
@@ -1,17 +1,7 @@
-import { push } from 'react-router-redux';
+import { goBack } from 'react-router-redux';
 import { Dashboard } from './containers';
 
-// export default {
-//   path: 'user',
-//   component: Dashboard,
-// };
-
 export default (store) => {
-  // injectReducer(store, { key: 'user', reducer: userReducer });
-  // injectReducer(store, { key: 'content', reducer: contentReducer });
-
-  // store.dispatch(onSignInAndOnSignOut);
-
   return {
     path: 'User',
     getComponent(nextState, cb) {
@@ -19,7 +9,7 @@ export default (store) => {
       if (user && user.email) {
         cb(null, Dashboard);
       } else {
-        store.dispatch(push('/'));
+        store.dispatch(goBack());
       }
     },
   };
