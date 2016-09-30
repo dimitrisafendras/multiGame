1139d55a176d36a118d9bcac5572904a9029ee45
dlivas <dimitris.livas@gmail.com>
2016-09-30 08:08:45 +0300

    fix server route redirection

--- server/agile-actor/lib/serve-spa-routes.js
+++ server/agile-actor/lib/serve-spa-routes.js
@@ -12,24 +12,21 @@ const sapRoutes = [
 export default (app) => {
   sapRoutes.forEach(route => app.use((ctx, next) => {
     if (ctx.url !== route) return next();
-    // ctx.path = route;
-    // ctx.redirect('/', route);
-    //
-    // ctx.type = 'html';
-    // ctx.body =
-    ctx.respond = false;
+    ctx.path = route;
+    ctx.redirect('/', route);
+    // ctx.respond = false;
 
-    const rs = fs.createReadStream(`${process.cwd()}/dist/index.html`);
-    const body = [];
-    rs
-    .on('error', (err) => console.error(err))
-    .on('data', (chunk) => body.push(chunk))
-    .on('end', () => {
-      ctx.res.writeHead(200, {
-        'Content-Type': 'text/html',
-        Location: route,
-      });
-      ctx.res.end(Buffer.concat(body).toString());
-    });
+    // const rs = fs.createReadStream(`${process.cwd()}/dist/index.html`);
+    // const body = [];
+    // rs
+    // .on('error', (err) => console.error(err))
+    // .on('data', (chunk) => body.push(chunk))
+    // .on('end', () => {
+    //   ctx.res.writeHead(200, {
+    //     'Content-Type': 'text/html',
+    //     Location: route,
+    //   });
+    //   ctx.res.end(Buffer.concat(body).toString());
+    // });
   }));
 };
