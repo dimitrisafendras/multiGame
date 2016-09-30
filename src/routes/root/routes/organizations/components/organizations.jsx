531d2a459e1a97577b438b7e06a8fee9cf9c2970
dlivas <dimitris.livas@gmail.com>
2016-09-30 08:08:02 +0300

    clean component props types

--- src/routes/root/routes/organizations/components/organizations.jsx
+++ src/routes/root/routes/organizations/components/organizations.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ValueProposition from './value-proposition/value-proposition';
 import Offerings from './offerings/offerings';
 
-//
-// Define the component
-//
 class Organizations extends React.Component {
   render() {
     return (
