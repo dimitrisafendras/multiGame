2da421f5bdc54ffaebd408b7f0a3452dbbc6df3d
Kostis Galoussis <konstantinos.galoussis@agileactors.com>
2016-10-16 21:10:52 +0300

    Refactor About page

--- src/routes/root/routes/about/components/agile-actors-vision/content.jsx
+++ src/routes/root/routes/about/components/agile-actors-vision/content.jsx
@@ -1,7 +1,4 @@
-//
-// The content of the component
-//
-export const content = {
+const content = {
   title: 'Our Vision',
   text: `We are tech professionals with a drive for a fulfilling career that will 
   help us discover our true talents and abilities. Our vision is to be an accessible, 
@@ -13,3 +10,5 @@ export const content = {
   We know that learning is a lifelong experience. We envision all tech professionals as 
   Agile Actors, able to succeed not just in work but also in life.`,
 };
+
+export default content;
