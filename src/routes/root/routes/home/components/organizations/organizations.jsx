36bb99dbe8686084b8e81d0ef0592b061c483306
dlivas <dimitris.livas@gmail.com>
2016-10-17 17:37:45 +0300

    update Organisation Container props

--- src/routes/root/routes/home/components/organizations/organizations.jsx
+++ src/routes/root/routes/home/components/organizations/organizations.jsx
@@ -7,90 +7,85 @@ import {
   Container,
   FlexContainer,
   Button,
-  Link
+  Link,
 } from 'components';
 
-const Organizations = () => {
-
-  return (
-    <article>
-      <Container
-        normalContainer
+const Organizations = () => (
+  <article>
+    <Container
+      normalContainer
+      className={classes.outerContainer}>
+      <Content
+        normalTitle
+        className={classes.outerTitle}>
+        {content.title}
+      </Content>
+      <Content
+        normalSubTitle
+        className={classes.subTitle}>
+        {content.subTitle}
+      </Content>
+      <FlexContainer
         center
-        column
-        className={classes.outerContainer}>
-        <Content
-          normalTitle
-          className={classes.outerTitle}>
-          {content.title}
-        </Content>
-        <Content
-          normalSubTitle
-          className={classes.subTitle}>
-          {content.subTitle}
-        </Content>
+        wrap>
         <FlexContainer
           center
-          wrap>
-          <FlexContainer
-            center
-            column
-            className={classes.container}>
-            <Content
-              image
-              className={classes.img}>
-              {content.organisations[0].Img}
-            </Content>
-            <Content
-              subTitle
-              className={classes.text}>
-              {content.organisations[0].content}
-            </Content>
-            <Button
-              primary
-              containerElement={<Link to={content.organisations[0].buttonRoute} />}
-              className={classes.button}>
-              {content.organisations[0].buttonLabel}
-            </Button>
-          </FlexContainer>
-
-          <FlexContainer
-            center
-            column
-            className={classes.innerTitleWrapper}>
-            <Content
-              text
-              className={classes.innerTitle}>
-              {content.subTitle}
-            </Content>
-            <div className={classes.divider} />
-          </FlexContainer>
-
-          <FlexContainer
-            center
-            column
-            className={classes.container}>
-            <Content
-              image
-              className={classes.img}>
-              {content.organisations[1].Img}
-            </Content>
-            <Content
-              subTitle
-              className={classes.text}>
-              {content.organisations[1].content}
-            </Content>
-            <Button
-              primary
-              containerElement={<Link to={content.organisations[1].buttonRoute} />}
-              className={classes.button}>
-              {content.organisations[1].buttonLabel}
-            </Button>
-          </FlexContainer>
+          column
+          className={classes.container}>
+          <Content
+            image
+            className={classes.img}>
+            {content.organisations[0].Img}
+          </Content>
+          <Content
+            subTitle
+            className={classes.text}>
+            {content.organisations[0].content}
+          </Content>
+          <Button
+            primary
+            containerElement={<Link to={content.organisations[0].buttonRoute} />}
+            className={classes.button}>
+            {content.organisations[0].buttonLabel}
+          </Button>
         </FlexContainer>
-      </Container>
-    </article>
-  );
-};
+
+        <FlexContainer
+          center
+          column
+          className={classes.innerTitleWrapper}>
+          <Content
+            text
+            className={classes.innerTitle}>
+            {content.subTitle}
+          </Content>
+          <div className={classes.divider} />
+        </FlexContainer>
+
+        <FlexContainer
+          center
+          column
+          className={classes.container}>
+          <Content
+            image
+            className={classes.img}>
+            {content.organisations[1].Img}
+          </Content>
+          <Content
+            subTitle
+            className={classes.text}>
+            {content.organisations[1].content}
+          </Content>
+          <Button
+            primary
+            containerElement={<Link to={content.organisations[1].buttonRoute} />}
+            className={classes.button}>
+            {content.organisations[1].buttonLabel}
+          </Button>
+        </FlexContainer>
+      </FlexContainer>
+    </Container>
+  </article>
+);
 
 export default Organizations;
