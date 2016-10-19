0e59ba5cfd3a814df85760c950b7132c007c640f
dlivas <dimitris.livas@gmail.com>
2016-10-19 18:11:05 +0300

    fix location origin for graphql for i.e.

--- src/model-services/server-apis/graphql.js
+++ src/model-services/server-apis/graphql.js
@@ -1,9 +1,18 @@
 import Lokka from 'lokka';
 import Transport from 'lokka-transport-http';
 
-const protocol = window.location.protocol;
-const host = window.location.host;
-const path = `${protocol}//${host}/graphql`;
+const originCreator = () => {
+  const protocol = window.location.protocol;
+  const hostname = window.location.hostname;
+  let port = window.location.port;
+  port = port ? `:${port}` : '';
+  return `${protocol}//${hostname}${port}`;
+};
+
+const origin = window.location.origin || originCreator();
+
+const path = `${origin}/graphql`;
+
 const transport = new Transport(path);
 
 const graphql = new Lokka({ transport });
