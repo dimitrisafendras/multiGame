eabcc84ce28f53c23f845952ea0b8cbe833b9840
dlivas <dimitris.livas@gmail.com>
2016-10-19 17:38:04 +0300

    fix graphql invocation

--- src/model-services/server-apis/graphql.js
+++ src/model-services/server-apis/graphql.js
@@ -1,8 +1,11 @@
 import Lokka from 'lokka';
 import Transport from 'lokka-transport-http';
 
-const graphql = new Lokka({
-  transport: new Transport('/graphql'),
-});
+const protocol = window.location.protocol;
+const host = window.location.host;
+const path = `${protocol}//${host}/graphql`;
+const transport = new Transport(path);
+
+const graphql = new Lokka({ transport });
 
 export default graphql;
