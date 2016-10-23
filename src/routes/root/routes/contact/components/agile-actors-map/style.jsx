58f6ea8ec300e6ea809337e165bcd6ded76f5260
dlivas <dimitris.livas@gmail.com>
2016-10-23 13:22:53 +0300

    update indentation

--- src/routes/root/routes/contact/components/agile-actors-map/style.jsx
+++ src/routes/root/routes/contact/components/agile-actors-map/style.jsx
@@ -1,9 +1,6 @@
 import muiTheme from 'styles/customized-mui-theme';
 import { jss } from 'components';
 
-//
-// Define the CSS styles of the component.
-//
 const styles = {
   container: {
     height: '360px',
@@ -18,8 +15,8 @@ const styles = {
     opacity: '0.8',
     backgroundColor: muiTheme.palette.white,
     boxShadow: '0 2px 11px 0 rgba(0, 0, 0, 0.25)',
-    padding:'9px 20px 17px 20px',
-},
+    padding: '9px 20px 17px 20px',
+  },
   link: {
     textDecoration: 'none',
     color: muiTheme.palette.textColor,
@@ -37,7 +34,7 @@ const styles = {
     lineHeight: '1.7',
     textDecoration: 'none',
   },
-  
+
   '@media (max-width: 768px)': {
     container: {
       height: '300px',
@@ -45,7 +42,7 @@ const styles = {
     textWrapper: {
       width: '196px',
       height: '62px',
-      padding:'7px 14px 15px 14px',
+      padding: '7px 14px 15px 14px',
     },
   },
   '@media (max-width: 320px)': {
@@ -58,10 +55,10 @@ const styles = {
       padding: '5px 0 5px 0',
     },
     title: {
-      lineHeight : '1',
+      lineHeight: '1',
     },
     text: {
-      lineHeight : '1',
+      lineHeight: '1',
     },
   },
 };
