25f31bab2825a4dd8ef3b8ddc01fc92e112102b1
Kostis Galoussis <konstantinos.galoussis@agileactors.com>
2016-10-23 21:02:14 +0300

    Remove test content

--- src/routes/root/routes/training/components/courses/content.jsx
+++ src/routes/root/routes/training/components/courses/content.jsx
@@ -95,26 +95,26 @@ const content = {
           aim at leaders and knowledge workers who are trying to be more agile and lean in their approach 
           to management. The courses and workshops typically draw a mix of team leaders, development managers, 
           directors, agile coaches, HR managers, project managers, and creative workers."`
-        },
-        {
-          id: '7',
-          title: 'Professional Scrum Trainings',
-          content: `Regarding the Agile Software Development our trainings include the Scrum framework, 
-          which is currently the most widely adopted Agile Framework for developing complex projects. <br /><br />
-          The following Professional Scrum Courses are available: <ul><li>Professional Scrum Foundations (PSF)</li>
-          <li>Professional Scrum Master (PSM)</li><li>Professional Scrum Product Owner (PSPO)</li></ul>
-          Our instructors are senior Scrum.org certified trainers and the certifications are also provided 
-          by the Scrum.org upon the successful completion of the appropriate online tests.`
-        },
-        {
-          id: '8',
-          title: 'Management 3.0',
-          content: `The Management 3.0 course targets organizations that face the challenge of an agile 
-          model and mindset transformation. <br /><br /> As described by Jurgen Apelo: " The Management 3.0 events 
-          aim at leaders and knowledge workers who are trying to be more agile and lean in their approach 
-          to management. The courses and workshops typically draw a mix of team leaders, development managers, 
-          directors, agile coaches, HR managers, project managers, and creative workers."`
-        },
+         },
+        // {
+        //   id: '7',
+        //   title: 'Professional Scrum Trainings',
+        //   content: `Regarding the Agile Software Development our trainings include the Scrum framework,
+        //   which is currently the most widely adopted Agile Framework for developing complex projects. <br /><br />
+        //   The following Professional Scrum Courses are available: <ul><li>Professional Scrum Foundations (PSF)</li>
+        //   <li>Professional Scrum Master (PSM)</li><li>Professional Scrum Product Owner (PSPO)</li></ul>
+        //   Our instructors are senior Scrum.org certified trainers and the certifications are also provided
+        //   by the Scrum.org upon the successful completion of the appropriate online tests.`
+        // },
+        // {
+        //   id: '8',
+        //   title: 'Management 3.0',
+        //   content: `The Management 3.0 course targets organizations that face the challenge of an agile
+        //   model and mindset transformation. <br /><br /> As described by Jurgen Apelo: " The Management 3.0 events
+        //   aim at leaders and knowledge workers who are trying to be more agile and lean in their approach
+        //   to management. The courses and workshops typically draw a mix of team leaders, development managers,
+        //   directors, agile coaches, HR managers, project managers, and creative workers."`
+        // },
       ],
     },
   ],
