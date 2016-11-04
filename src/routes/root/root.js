e899eb33cf15c375805333cd0b79b4b8e9ac5c73
dlivas <dimitris.livas@gmail.com>
2016-11-04 11:11:30 +0200

    update google analytics management

--- src/routes/root/root.js
+++ src/routes/root/root.js
@@ -1,3 +1,5 @@
+import ReactGA from 'react-ga';
+
 // We only need to import the modules necessary for initial render
 import {
   HomeRoute,
@@ -21,6 +23,8 @@ import contentReducer from './modules/content/content-reducer';
 import { onSignInAndOnSignOut } from './modules/user/user-actions';
 import { contentUpdate } from './modules/content/content-actions';
 
+ReactGA.initialize('UA-86819304-1');
+
 export const rootRoute = (store) => {
   injectReducer(store, { key: 'user', reducer: userReducer });
   injectReducer(store, { key: 'content', reducer: contentReducer });
@@ -37,6 +41,19 @@ export const rootRoute = (store) => {
   // Initialize static content
   //
   store.dispatch(contentUpdate);
+
+  //
+  // Subscribe each page view to google analytics
+  //
+  let pathname = '';
+  store.subscribe(() => {
+    let previousPathname = pathname;
+    const { router } = store.getState();
+    pathname = router.locationBeforeTransitions.pathname;
+    if (previousPathname !== pathname) {
+      ReactGA.pageview(pathname);
+    }
+  });
 
   return {
     path: '/',
