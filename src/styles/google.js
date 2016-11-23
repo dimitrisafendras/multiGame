f8913640effb78d039e0b887f5834cc04e9654ad
dlivas <dimitris.livas@gmail.com>
2016-11-23 23:17:29 +0200

    add webfontloader
    add webfont loader remove google font css from index.html

---
+++ src/styles/google.js
@@ -0,0 +1,10 @@
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: [
+      'Lato:400,300,700,900,100',
+      'Material+Icons',
+    ],
+  },
+});
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,7 +1,8 @@
 // Include project styles
 
+import './google';
 import 'normalize.css/normalize.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import 'font-awesome/css/font-awesome.css';
-import 'styles/css/core.css';
+import './css/core.css';
