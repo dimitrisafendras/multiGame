b289b9fa59580c2d385343e5141dd66f30f5e44f
dlivas <dimitris.livas@gmail.com>
2016-12-20 15:54:32 +0200

    update package.json

--- src/routes/root/routes/careers/index.js
+++ src/routes/root/routes/careers/index.js
@@ -1,6 +1,23 @@
-import Careers from './components/careers';
+// import Careers from './components/careers';
 
 export default {
   path: 'Careers',
-  component: Careers,
+  // component: Careers,
+  getComponent(nextState, cb) {
+    // Webpack - use 'require.ensure' to create a split point
+    // and embed an async module loader (jsonp) when bundling
+    require.ensure([], (require) => {
+      // Webpack - use require callback to define
+      // dependencies for bundling
+      const Careers = require('./components/careers').default;
+      // /* const reducer = require('./modules/dashpord').default
+      // Add the reducer to the store on key 'counter'
+      // /* injectReducer(store, { key: 'dashboard', reducer })
+
+      // Return getComponent
+      cb(null, Careers);
+
+      /* Webpack named bundle   */
+    }, 'careers');
+  },
 };
