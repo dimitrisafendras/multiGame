3cc1231e1028cc413ee7aeddc84e55a5bf84eaf8
dlivas <dimitris.livas@gmail.com>
2016-12-20 10:20:19 +0200

    add flow type in client-config

--- src/model-services/models/client-config/socketio/client-config.js
+++ src/model-services/models/client-config/socketio/client-config.js
@@ -1,11 +1,11 @@
 import { clientConfigSocket } from 'model-services/server-apis';
 
+type OnClientConfig = (config?: Object) => void;
+
 /*
  * Register Listeners (observers) of the asynchronous changes
  * of the model-service clientConfig state.
  */
-export const setOnClientConfig = (
-  onClientConfig: (config?: Object) => void
-) => {
+export const setOnClientConfig = (onClientConfig: OnClientConfig) => {
   clientConfigSocket.on('clientConfig', onClientConfig);
 };
