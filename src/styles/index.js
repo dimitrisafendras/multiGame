aa9832b897bb20e1917ee4cb7797d6dcc3f1f355
dlivas <dimitris.livas@gmail.com>
2016-12-20 16:58:43 +0200

    update packages

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,6 @@
 // Include project styles
 
-import 'normalize.css/normalize.css';
+import 'normalize.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import 'font-awesome/css/font-awesome.css';
