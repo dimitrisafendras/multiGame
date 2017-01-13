9c6a573367086eca8495de3179f287083b49c907
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-01-13 13:01:08 +0200

    init

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -6,6 +6,7 @@ import hangman from './hangmanReducer'
 import home from './homeReducer'
 import { Simon } from './simonReducer'
 import memory from './memoryReducer'
+import client from '../../src/modules/client-config/client-config-reducer'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
