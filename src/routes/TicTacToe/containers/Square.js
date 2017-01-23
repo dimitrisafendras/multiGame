18cea9ced1008b99bbe21a0e9adf6759c941ac38
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-01-23 14:54:47 +0200

    FIX card game

--- src/routes/TicTacToe/containers/Square.js
+++ src/routes/TicTacToe/containers/Square.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { markTile } from '../modules/actions/index';
+import { markTile } from '../modules/actions';
 
 
 export function Square({ id, squares, markTile, currentPlayer, victory, gameStyle }) {
