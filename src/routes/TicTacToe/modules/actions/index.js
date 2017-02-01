a345fc5118836c38bc00187dd0cba5552116ca9c
jim <dimitris.afendras@gmail.com>
2017-02-01 14:13:42 +0200

    pls

--- src/routes/TicTacToe/modules/actions/index.js
+++ src/routes/TicTacToe/modules/actions/index.js
@@ -4,10 +4,12 @@ const MARK_TILE = 'MARK_TILE';
 const NEW_GAME = 'NEW_GAME';
 const UNDO_MOVE = 'UNDO_MOVE';
 
-export const markTile = (player, key)=> ({
+export const offline = {
+  markTile: (player, key) => ({
     type: MARK_TILE,
-    payload: { player: player, key: key }
-});
+    payload: {player: player, key: key}
+  })
+};
 
 export const newGame = (player, gameStyle)=> ({
     type: NEW_GAME,
