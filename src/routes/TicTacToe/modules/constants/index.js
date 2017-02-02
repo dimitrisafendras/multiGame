71138ed7222aa757abc8eb3f30d5d5b362011f2c
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-02 11:34:58 +0200

    FIX cardGame

--- src/routes/TicTacToe/modules/constants/index.js
+++ src/routes/TicTacToe/modules/constants/index.js
@@ -1,11 +1,10 @@
 'use strict';
-import { offline } from '../actions';
+import { options } from '../actions';
 
 //action constants
 export const MARK_TILE = 'MARK_TILE';
 export const NEW_GAME = 'NEW_GAME';
-export const UNDO_MOVE = 'UNDO_MOVE';
 
 //action creator constants
-export const markTile = offline.markTile;
-export const newGame = offline.newGame;
+export const markTile = options.offline.markTile;
+export const newGame = options.offline.newGame;
