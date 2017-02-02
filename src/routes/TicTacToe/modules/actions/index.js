fceee1070e69616f77cbf598c8869568137aac6b
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-02 11:34:58 +0200

    FIX cardGame

--- src/routes/TicTacToe/modules/actions/index.js
+++ src/routes/TicTacToe/modules/actions/index.js
@@ -3,16 +3,18 @@
 import { MARK_TILE } from '../constants'
 import { NEW_GAME } from '../constants'
 
-export const offline = {
-  markTile: (player, key) => ({
-    type: MARK_TILE,
-    payload: {player: player, key: key}
-  }),
+export const options = {
 
-  newGame: (player, gameStyle)=> ({
-    type: NEW_GAME,
-    payload: { player: player, gameStyle }
-  })
+  offline: {
+    markTile: (player, key) => ({
+      type: MARK_TILE,
+      payload: {player: player, key: key}
+    }),
+    newGame: (player, gameStyle) => ({
+      type: NEW_GAME,
+      payload: {player: player, gameStyle}
+    })
+  }
 };
 
 
