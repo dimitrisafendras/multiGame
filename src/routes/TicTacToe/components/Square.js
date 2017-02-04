e3108a98ba177a90effae415a3fa7f4ec9e5493e
jim <dimitris.afendras@gmail.com>
2017-02-04 13:14:01 +0200

    made square and online square copm as one

--- src/routes/TicTacToe/components/Square.js
+++ src/routes/TicTacToe/components/Square.js
@@ -1,24 +1,54 @@
 import React from 'react';
+import { ticTacToeSocket } from '../../../model-services/server-apis';
 
-export const Square = ({ id, squares, markTile, currentPlayer, victory, gameStyle })=> {
+export const Square = ({ id, squares, markTile, markTileOnline, currentPlayer, victory, gameStyle })=> {
 
   const selectTile = ()=> {
-    if ( !squares[id] && !victory ) {
-      return  markTile(currentPlayer, id);
+    if (!squares[id] && !victory) {
+      return markTile(currentPlayer, id);
     }
   };
 
-  const clickable = ()=> {
-    if ( gameStyle == 'pvp' || currentPlayer == 'x' ){
+  const selectTileOnline = ()=>{
+    if (!squares[id] && !victory) {
+      return markTileOnline(currentPlayer, id);
+    }
+  };
+
+  const clickable = () => {
+    if (gameStyle == 'pvp' || currentPlayer == 'x') {
       return 'clickable'
     }
   };
 
-  return (
-    <div className={clickable()} onClick={()=>
-    {selectTile()}
-    }>
-      {squares[id]}
-    </div>
-  )
+
+  if (gameStyle == 'pvp' || gameStyle == 'pvb') {
+    return (
+      <div className={clickable()} onClick={() => {
+        selectTile()
+      }
+      }>
+        {squares[id]}
+      </div>
+    );
+  }
+  else if (gameStyle == 'Opvp'){
+    return (
+      <div className={clickable()} onClick={()=>
+      {ticTacToeSocket.emit('markTile', selectTileOnline())}
+      }>
+        {squares[id]}
+      </div>
+    )
+  }
+  else{
+    return (
+      <div className={clickable()} onClick={() => {
+        selectTile()
+      }
+      }>
+        {squares[id]}
+      </div>
+    );
+  }
 };
--- src/routes/TicTacToe/containers/Square.js
+++ src/routes/TicTacToe/containers/Square.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { markTile } from '../modules/constants';
+import { markTileOnline } from '../modules/constants';
 import { Square } from '../components/Square';
+import { ticTacToeSocket } from '../../../model-services/server-apis';
+import { store } from 'main';
 
 const mapStateToProps = (state)=> ({
     squares: state.game.squares,
@@ -10,5 +13,9 @@ const mapStateToProps = (state)=> ({
     gameStyle: state.game.gameStyle,
 });
 
-export default connect(mapStateToProps, {markTile} )(Square);
+export default connect(mapStateToProps, {markTile, markTileOnline} )(Square);
 
+ticTacToeSocket.on('markTile', (action)=> {
+  console.log('XXXXXXXXXXXXXXXXXXXX', action);
+  store.dispatch(action);
+});
