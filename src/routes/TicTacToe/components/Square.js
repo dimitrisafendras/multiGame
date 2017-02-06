60f9e3c11b2c238603f5fdbaf548ea39f13c3ed5
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-06 12:35:53 +0200

    separate sockets from components
    make more compact compnents

--- src/routes/TicTacToe/components/Square.js
+++ src/routes/TicTacToe/components/Square.js
@@ -3,18 +3,18 @@ import { ticTacToeSocket } from '../../../model-services/server-apis';
 
 export const Square = ({ id, squares, markTile, markTileOnline, currentPlayer, victory, gameStyle })=> {
 
+  const clickable = () => {
+    if (gameStyle == 'pvp' || currentPlayer == 'x') {
+      return 'clickable'
+    }
+  };
+
   const selectTile = (gameStyle)=> {
     if (!squares[id] && !victory && gameStyle == 'Opvp') {
       return markTileOnline(currentPlayer, id);
     }
     else if (!squares[id] && !victory) {
       return markTile(currentPlayer, id);
-    }
-  };
-
-  const clickable = () => {
-    if (gameStyle == 'pvp' || currentPlayer == 'x') {
-      return 'clickable'
     }
   };
 
