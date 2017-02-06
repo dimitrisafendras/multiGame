aad30043250c566478eecc61591fc3b4f488c96b
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-06 12:12:27 +0200

    separate sockets from components
    make more compact compnents

--- src/routes/TicTacToe/components/Square.js
+++ src/routes/TicTacToe/components/Square.js
@@ -3,15 +3,12 @@ import { ticTacToeSocket } from '../../../model-services/server-apis';
 
 export const Square = ({ id, squares, markTile, markTileOnline, currentPlayer, victory, gameStyle })=> {
 
-  const selectTile = ()=> {
-    if (!squares[id] && !victory) {
-      return markTile(currentPlayer, id);
-    }
-  };
-
-  const selectTileOnline = ()=>{
-    if (!squares[id] && !victory) {
+  const selectTile = (gameStyle)=> {
+    if (!squares[id] && !victory && gameStyle == 'Opvp') {
       return markTileOnline(currentPlayer, id);
+    }
+    else if (!squares[id] && !victory) {
+      return markTile(currentPlayer, id);
     }
   };
 
@@ -24,14 +21,14 @@ export const Square = ({ id, squares, markTile, markTileOnline, currentPlayer, v
   if (gameStyle == 'Opvp'){
     return (
       <div className={clickable()} onClick={()=>
-      {ticTacToeSocket.emit('markTile', selectTileOnline())}}>
+      {ticTacToeSocket.emit('markTile', selectTile(gameStyle))}}>
         {squares[id]}
       </div>
     )
   }
   else{
     return (
-      <div className={clickable()} onClick={() => {selectTile()}}>
+      <div className={clickable()} onClick={() => {selectTile(gameStyle)}}>
         {squares[id]}
       </div>
     )
