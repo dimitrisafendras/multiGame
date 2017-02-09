4be618667c057589675770760e444f6e2b36c507
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-09 10:17:22 +0200

    :)

--- src/routes/TicTacToe/containers/TicTacToe.js
+++ src/routes/TicTacToe/containers/TicTacToe.js
@@ -2,7 +2,6 @@ import TicTacToe from '../components/TicTacToe';
 import { connect } from 'react-redux';
 import { newGame, newGameOnline } from '../modules/constants';
 import { onNewGame } from '../../../model-services/ticTacToe/socketio/ticTacToe';
-import { store } from 'main';
 
 export default connect(
   (state) => {
