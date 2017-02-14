40391d315abbba14138d836ce789a86655fbc661
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-14 14:26:51 +0200

    :)

--- src/model-services/ticTacToe/socketio/ticTacToe.js
+++ src/model-services/ticTacToe/socketio/ticTacToe.js
@@ -1,10 +1,14 @@
 import { ticTacToeSocket } from '../../../model-services/server-apis';
 import { dispatcher } from '../../../routes/TicTacToe/modules/functions';
 
-ticTacToeSocket.on('markTile', (action)=> (
-  dispatcher(action)
-));
+export const onMarkTile = ()=> ({
+  onMarkTile: ticTacToeSocket.on('markTile', (action)=> (
+    dispatcher(action)
+  ))
+});
 
-ticTacToeSocket.on('newGame', (action)=> (
-  dispatcher(action)
-));
+export const onNewGame = ()=> ({
+  onNewGame: ticTacToeSocket.on('newGame', (action)=> (
+    dispatcher(action)
+  ))
+});
