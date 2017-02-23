4c527da9db4780670ec10ebfd1bc73ef13382fb2
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-02-23 13:26:03 +0200

    :)

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -27,5 +27,4 @@ export const injectReducer = (store, { key, reducer }) => {
   store.asyncReducers[key] = reducer;
   store.replaceReducer(makeRootReducer(store.asyncReducers))
 };
-
 export default makeRootReducer
