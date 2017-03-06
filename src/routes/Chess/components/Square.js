5b8eee5fbf6d3012b41da691b17e29fa72786774
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-06 12:21:30 +0200

    :)

---
+++ src/routes/Chess/components/Square.js
@@ -0,0 +1,11 @@
+import styles from './styles.scss'
+
+export const Square = ({ id, squares, markTile, currentPlayer, victory })=> {
+
+  return (
+    <div className= styles.>
+
+    </div>
+  )
+
+};
