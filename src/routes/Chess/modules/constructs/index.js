05bfc26ce4b6efa0bb31262c3376ed6f4d4386f0
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-07 13:21:07 +0200

    :)

--- src/routes/Chess/modules/constructs/index.js
+++ src/routes/Chess/modules/constructs/index.js
@@ -1,6 +1,22 @@
+import {
+  wKing, wQueen, wRook, wBishop,
+  wKnight, wPawn, bKing, bQueen,
+  bRook, bBishop, bKnight, bPawn
+} from '../../components/chessPieces'
 
 const figures = new Map([
-  ['king', king],
+  ['wKing', wKing],
+  ['bKing', bKing],
+  ['wQueen', wQueen],
+  ['bQueen', bQueen],
+  ['wRook', wRook],
+  ['bRook', bRook],
+  ['wBishop', wBishop],
+  ['bBishop', bBishop],
+  ['wKnight', wKnight],
+  ['bKnight', bKnight],
+  ['wPawn', wPawn],
+  ['bPawn', bPawn],
 ]);
 
 export default (key)=> (figures.get(key));
