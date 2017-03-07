602a1db8f4757a33688c4026528bc2656aa10ea7
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-07 13:11:16 +0200

    :)

--- src/routes/Chess/modules/constructs/index.js
+++ src/routes/Chess/modules/constructs/index.js
@@ -1,15 +1,6 @@
-import React from 'react'
-
-const tmp = (()=> {
-  return (
-  <div>
-    &#9812;
-  </div>
-  )
-})();
 
 const figures = new Map([
-  ['king', tmp],
+  ['king', king],
 ]);
 
 export default (key)=> (figures.get(key));
