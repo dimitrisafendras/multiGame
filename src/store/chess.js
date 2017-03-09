220c630791dd846d51651638b0c242295a6f3699
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-09 14:16:54 +0200

    :)

--- src/store/chess.js
+++ src/store/chess.js
@@ -1,5 +1,12 @@
-import {initialState, SELECT_TILE} from '../routes/Chess/modules/constants';
+import { SELECT_TILE } from '../routes/Chess/modules/constants';
 import selectedATile from '../routes/Chess/modules/functions/selectedTile';
+import squares from '../routes/Chess/modules/functions';
+
+const initialState = {
+  round: 1,
+  squares,
+  selectedTile:[undefined, undefined],
+};
 
 export default function chess(state = initialState, action) {
   switch (action.type){
