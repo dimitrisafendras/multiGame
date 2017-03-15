2b23646846691a2669dee6a698e9b8573b78254b
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-15 11:39:58 +0200

    :)

--- src/routes/Chess/modules/functions/checkDirections.js
+++ src/routes/Chess/modules/functions/checkDirections.js
@@ -3,30 +3,28 @@ import { MAX_COLS, MAX_ROWS } from '../constants';
 
 const checkDirections = (selLineID, selSquareID, squares, x, y, boolBoard)=> {
 
-  let targetLine = squares[selLineID+x];
-  let selectSquare = squares[selLineID][selSquareID];
-  let targetSquare = squares[selLineID + x][selSquareID + y];
-  let thisFigure = figureIs(selectSquare, targetSquare);
+  let thisFigure;
   let noFigureBetween = true;
   let dirX = x.valueOf();
   let dirY = y.valueOf();
 
   while((selLineID + x <= MAX_ROWS) && (selSquareID + y <= MAX_COLS) && (noFigureBetween)){
-    if(!(targetLine) || !(targetSquare) ){
+
+    if(!(squares[selLineID+x]) || !(squares[selLineID + x][selSquareID +y]) ){
       return boolBoard;
     }
+
+    thisFigure = figureIs(squares[selLineID][selSquareID], squares[selLineID + x][selSquareID + y]);
 
     if(thisFigure === 'EMPTY'){
       boolBoard[selLineID + x][selSquareID + y] = true;
       x += dirX;
       y += dirY;
     }
-
     else if (thisFigure === 'FOE'){
       boolBoard[selLineID + x][selSquareID + y] = true;
       noFigureBetween = false;
     }
-
     else if (thisFigure === 'ALLY'){
       noFigureBetween = false;
     }
--- src/routes/Chess/modules/functions/figureIs.js
+++ src/routes/Chess/modules/functions/figureIs.js
@@ -1,5 +1,6 @@
 
 const figureIs  = (selectedFigure, targetFigure)=> {
+  if (!targetFigure) return 'ERROR';
   if (selectedFigure.charAt(0) === targetFigure.charAt(0)) {
     return 'ALLY'
   }
