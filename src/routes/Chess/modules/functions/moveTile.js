ef387991202234017666bb88daa8639f88e7a961
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-16 12:04:46 +0200

    REFACTOR

--- src/routes/Chess/modules/functions/moveTile.js
+++ src/routes/Chess/modules/functions/moveTile.js
@@ -2,11 +2,13 @@ import { MAX_COLS, CREATE_BOOL_BOARD, DEFAULT_TILE, NEXT_ROUND} from '../constan
 //import { ally, enemy } from '../functions'; //FIX
 
 const moveTile = (state, {lineId, squareId})=> {
-
-  if (true) {
+    let victory = false;
     let newSquares = state.squares.slice(0, MAX_COLS);
     let lID = state.selectedTile.line;
     let sID = state.selectedTile.square;
+    if (state.squares[lineId][squareId].substring(1) === 'King'){
+      victory = state.round;
+    }
     newSquares[lineId][squareId] = state.squares[lID][sID];
     newSquares[lID][sID] = 'empty';
     const newBoolBoard = CREATE_BOOL_BOARD();
@@ -16,8 +18,8 @@ const moveTile = (state, {lineId, squareId})=> {
       selectedTile: DEFAULT_TILE,
       boolBoard: newBoolBoard,
       round: NEXT_ROUND[state.round],
+      victory,
     }
-  }
 };
 
 export default moveTile;
