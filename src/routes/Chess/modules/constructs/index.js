627ebae471ece364b7dde5ef0ad23b2f95ce6007
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-20 12:06:27 +0200

    REFACTOR

--- src/routes/Chess/modules/constructs/index.js
+++ src/routes/Chess/modules/constructs/index.js
@@ -1,19 +1,7 @@
-import * as img from '../../components/chessPieces'
+import directions from './directions'
+import figures from './figuresMap'
 
-export const figures = new Map([
-  ['wKing', img.wKing],
-  ['bKing', img.bKing],
-  ['wQueen', img.wQueen],
-  ['bQueen', img.bQueen],
-  ['wRook', img.wRook],
-  ['bRook', img.bRook],
-  ['wBishop', img.wBishop],
-  ['bBishop', img.bBishop],
-  ['wKnight', img.wKnight],
-  ['bKnight', img.bKnight],
-  ['wPawn', img.wPawn],
-  ['bPawn', img.bPawn],
-  ['empty',img.Empty],
-]);
-
-export default (key)=> (figures.get(key));
+export {
+  directions,
+  figures
+}
--- src/routes/Chess/modules/functions/moveOptions/moveBishop.js
+++ src/routes/Chess/modules/functions/moveOptions/moveBishop.js
@@ -1,15 +1,13 @@
 import checkDirections from '../checkDirections';
 import { CREATE_BOOL_BOARD } from '../../constants';
+import { directions } from '../../constructs';
 
 const moveBishop = (selLineID, selSquareID, squares)=> {
   const boolBoard = CREATE_BOOL_BOARD();
-  checkDirections(selLineID, selSquareID, squares, 1, 1, boolBoard);
-  checkDirections(selLineID, selSquareID, squares, -1, 1, boolBoard);
-  checkDirections(selLineID, selSquareID, squares, 1, -1, boolBoard);
-  checkDirections(selLineID, selSquareID, squares, -1, -1, boolBoard);
-
+  for(let vector of directions.bishop) {
+      checkDirections(selLineID, selSquareID, squares, vector.x, vector.y, boolBoard);
+  }
   return boolBoard;
-
 };
 
 export default moveBishop;
