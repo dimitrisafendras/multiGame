134a928d15a6bb4b0c5438b97d8e76851cba6cba
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-20 13:23:21 +0200

    REFACTOR

--- src/routes/Chess/modules/constructs/directions.js
+++ src/routes/Chess/modules/constructs/directions.js
@@ -18,22 +18,22 @@ const directions = {
     {x:-1, y:0}, {x:0, y:-1},
   ],
 
-  pawn: {
-    attack:[
-      {x: 1, y: 1},
-      {x: 1, y: -1},
-    ],
-    move:[
-      {x: 1, y: 0}
-    ],
-  },
-
   knight:[
     {x:1, y:2}, {x:1, y:-2},
     {x:-1, y:2}, {x:-1, y:-2},
     {x:2, y:1}, {x:2, y:-1},
     {x:-2, y:1}, {x:-2, y:-1},
   ],
+
+  pawn: {
+    attack:[
+      {x: 1, y: 1},
+      {x: 1, y: -1},
+    ],
+    move: {x: 1, y: 0},
+
+    },
+
 };
 
 export default directions;
--- src/routes/Chess/modules/functions/moveOptions/movePawn.js
+++ src/routes/Chess/modules/functions/moveOptions/movePawn.js
@@ -7,8 +7,9 @@ const movePawn = (selLineID, selSquareID, squares, round)=> {
   let thisPawn = PAWN_OPTIONS[round.charAt(0)];
   let pawnSide = thisPawn.direction;
   let pawnStartLine = thisPawn.startLine;
-
+  let moveVector = directions.pawn.move.x * pawnSide;
   const boolBoard = CREATE_BOOL_BOARD();
+
   for(let vector of directions.pawn.attack) {
     if (squares[selLineID + vector.x * pawnSide] && squares[selLineID + vector.x * pawnSide][selSquareID + vector.y]){
       if((figureIs(squares[selLineID][selSquareID], squares[selLineID + vector.x * pawnSide][selSquareID + vector.y]) === 'FOE' )) {
@@ -17,15 +18,13 @@ const movePawn = (selLineID, selSquareID, squares, round)=> {
     }
   }
 
-  for(let vector of directions.pawn.move) {
-    if (squares[selLineID + vector.x * pawnSide] && squares[selLineID + vector.x * pawnSide][selSquareID + vector.y]){
-      if((figureIs(squares[selLineID][selSquareID], squares[selLineID + vector.x * pawnSide][selSquareID + vector.y]) === 'EMPTY' )) {
-        boolBoard[selLineID + vector.x * pawnSide][selSquareID + vector.y] = true;
-      }
-      if(selLineID === pawnStartLine){
-        if((figureIs(squares[selLineID][selSquareID], squares[selLineID + vector.x * pawnSide * 2][selSquareID + vector.y]) === 'EMPTY' )) {
-          boolBoard[selLineID + vector.x * pawnSide * 2][selSquareID + vector.y] = true;
-        }
+  if (squares[selLineID + moveVector] && squares[selLineID + moveVector][selSquareID]){
+    if((figureIs(squares[selLineID][selSquareID], squares[selLineID + moveVector][selSquareID]) === 'EMPTY' )) {
+      boolBoard[selLineID + moveVector][selSquareID] = true;
+    }
+    if(selLineID === pawnStartLine){
+      if((figureIs(squares[selLineID][selSquareID], squares[selLineID + moveVector * 2][selSquareID]) === 'EMPTY' )) {
+        boolBoard[selLineID + moveVector * 2][selSquareID] = true;
       }
     }
   }
