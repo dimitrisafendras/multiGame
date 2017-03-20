ea99e183675799316d694c9048f28824156483fb
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-03-20 13:27:00 +0200

    REFACTOR

--- src/routes/Chess/modules/functions/toggleOff.js
+++ src/routes/Chess/modules/functions/toggleOff.js
@@ -1,10 +1,8 @@
 import { CREATE_BOOL_BOARD, DEFAULT_TILE } from '../constants';
 
 const toggleOff = (state)=> {
-
   return {...state, selectedTile:DEFAULT_TILE, boolBoard : CREATE_BOOL_BOARD() }
 };
-
 
 export default toggleOff;
 
