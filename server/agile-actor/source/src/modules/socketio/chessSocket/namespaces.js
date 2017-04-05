65e450b10535be5ddb9ae38608d1eb77e15e8760
christsop <p3130211@dias.aueb.gr>
2017-04-05 11:58:44 +0300

    remove from player list when reset some styling

--- server/agile-actor/source/src/modules/socketio/chessSocket/namespaces.js
+++ server/agile-actor/source/src/modules/socketio/chessSocket/namespaces.js
@@ -1,35 +1,42 @@
 export default (io) => {
-
   const sessionIo = io.of('/chessSocket');
   let white = '';
   let black = '';
   let users = [];
   let players = new Map();
 
-  sessionIo.on('connection', (socket)=> {
+  const getKeyByValue = (players, socket, users) => {
+    for(let i=0; i<players.size; i++){
+      if(players.get(users[i]) === socket){
+        return users[i];
+      }
+    }
+  };
+
+  sessionIo.on('connection', (socket) => {
     let player = '';
     let opponent = '';
     console.log('  --> SocketIO on connection CHESS');
 
-    socket.on('challengePlayer', (users)=> {
+    socket.on('challengePlayer', (users) => {
       player = players.get(users.player);
       opponent = players.get(users.opponent);
       opponent.emit('gotChallenged', users.player);
     });
 
-    socket.on('moveTile', (action)=> {
+    socket.on('moveTile', (action) => {
       player.emit('movedTile', action);
       opponent.emit('movedTile', action);
     });
 
-    socket.on('readyToPlay', (users)=> {
+    socket.on('readyToPlay', (users) => {
       player = players.get(users.player);
       opponent = players.get(users.opponent);
       player.emit('gotReady',{playerColor: 'white'});
       opponent.emit('gotReady',{playerColor: 'black'})
     });
 
-    socket.on('getReady',(username)=> {
+    socket.on('getReady',(username) => {
       if (username){
         players.set(username, socket);
         users.push(username);
@@ -42,11 +49,19 @@ export default (io) => {
       }
     });
 
-    socket.on('getDisconnected', (username)=> {
+    socket.on('getDisconnected', (username) => {
       players.delete(username);
       const indexToRemove = users.indexOf(username);
       users.splice(indexToRemove, 1);
       socket.broadcast.emit('updatePlayers', users);
     });
+
+    socket.on('disconnect', () => {
+      const userToRemove = getKeyByValue(players, socket, users);
+      const indexToRemove = users.indexOf(userToRemove);
+      users.splice(indexToRemove, 1);
+      socket.broadcast.emit('updatePlayers', users);
+    });
+
   });
 };
