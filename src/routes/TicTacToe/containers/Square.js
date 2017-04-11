22d576ad605807e738d2896ff78ad8334ea8ca14
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-04-11 14:05:50 +0300

    refactoring stuff

--- src/routes/TicTacToe/containers/Square.js
+++ src/routes/TicTacToe/containers/Square.js
@@ -3,11 +3,14 @@ import { connect } from 'react-redux';
 import { markTile, markTileEvent } from '../modules/constants';
 import { Square } from '../components/Square';
 
-const mapStateToProps = (state)=> ({
+const mapStateToProps = (state)=> {
+  console.log(state.game.gameStyle);
+  return ({
     squares: state.game.squares,
     currentPlayer: state.players.current_turn,
     victory: state.game.victory,
     gameStyle: state.game.gameStyle,
-});
+  });
+}
 
 export default connect(mapStateToProps, { markTile, markTileEvent })(Square);
