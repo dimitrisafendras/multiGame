2fa75e867d4fd014a188262f47a44d4becdf5997
Dimitris Afendras <dimitris.afendras@gmail.com>
2017-04-20 11:26:59 +0300

    refactoring stuff

--- server/agile-actor/source/src/modules/socketio/Simon/namespaces.js
+++ server/agile-actor/source/src/modules/socketio/Simon/namespaces.js
@@ -8,34 +8,28 @@ export default (io) => {
     console.log('  --> SocketIO on connection', id);
     players.push(id);
     socket.broadcast.emit('connected', players.length);
-    console.log('Online players: '+players.length);
-
-    console.log('online players: '+ players.length+'ready '+ ready)
 
     //ON DISCONNECT
     socket.on('disconnect', ()=>{
       console.log('disconected');
       players.shift();
       if (ready>0) ready--;
-      console.log('online players: '+ players.length+'ready '+ ready)
       socket.broadcast.emit('onlinePlayers', players.length);
     });
 
     socket.on('clickedPad', (pad)=>{
-      console.log('wwwwwwwwwwwwwww', pad);
       socket.broadcast.emit('clickedPad', (pad));
       socket.emit('clickedPad', (pad));
     });
 
     socket.on('nextLevel', ()=>{
-      console.log('next level');
       let move = Math.floor(Math.random() * 4);
       socket.emit('nextLevel', move);
       socket.broadcast.emit('nextLevel', move);
-    })
+    });
+
     socket.on('ready', ()=>{
       ready++;
-      console.log(ready);
       if(ready>1){
         let move = Math.floor(Math.random() * 4);
         socket.emit('allReady', move);
